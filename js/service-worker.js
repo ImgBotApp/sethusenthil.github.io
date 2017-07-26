2cb4adf59be285a83613fbbb6b1ac69e294b7766
Sethu Senthil <Sethu@MacBook-Pro.home>
2017-07-26 17:37:57 -0400

    ‏‎‎‏‏‎‎‏‏‎‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎

--- js/service-worker.js
+++ js/service-worker.js
@@ -100,3 +100,27 @@ self.addEventListener('fetch', event => {
   // event.respondWith(). If no fetch handlers call event.respondWith(), the request will be
   // handled by the browser as if there were no service worker involvement.
 });
+});
+var isPushEnabled = false;
+
+…
+
+window.addEventListener('load', function() {
+  var pushButton = document.querySelector('.js-push-button');
+  pushButton.addEventListener('click', function() {
+    if (isPushEnabled) {
+      unsubscribe();
+    } else {
+      subscribe();
+    }
+  });
+
+  // Check that service workers are supported, if so, progressively
+  // enhance and add push messaging support, otherwise continue without it.
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(initialiseState);
+  } else {
+    console.warn('Service workers aren\'t supported in this browser.');
+  }
+});
