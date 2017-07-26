24b7f904e13e06603319885dd0a4f7f2d0f8ccaf
Sethu Senthil <Sethu@MacBook-Pro.home>
2017-07-26 16:03:48 -0400

    ‏‎‎‏‏‎‎‏‏‎‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎‏‏‎

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-Push.create("Hello world!", {
-    body: "How's it hangin'?",
-    icon: 'media/favicon.png',
-    timeout: 4000,
-    onClick: function () {
-        window.focus();
-        this.close();
-    }
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
